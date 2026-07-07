f7bd45fa393d492935562cb46aed4356787c2baa
hadi <hadi.deris.h82@gmail.com>
2026-07-07 22:22:57 +0330

    Add dashboard menu links and ProfileController
    Added new navigation links for invoices, cart, edit info, and password change to the dashboard menu. Introduced ProfileController with an Index action in the UserPanel area.

---
+++ store.web/Areas/UserPanel/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace store.web.Areas.UserPanel.Controllers
+{
+    public class ProfileController : UserPanelBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
